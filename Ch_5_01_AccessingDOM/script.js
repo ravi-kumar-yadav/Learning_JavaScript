a8e1c376dda869a08885df94f65f326dab96991c
Ravi Kumar Yadav <ravi.k.yadav108@gmail.com>
2016-02-05 15:38:42 +0530

    performed getElementById('id')

--- Ch_5_01_AccessingDOM/script.js
+++ Ch_5_01_AccessingDOM/script.js
@@ -1,2 +1,10 @@
 
+// get anchor tag with id "mainTitle" from html page
+var mainTitle = document.getElementById('mainTitle');
+
+// NodeType --> Element (1)
+console.log("This is an element of type: ", mainTitle.nodeType);
+console.log("Inner HTML is ", mainTitle.innerHTML);
+console.log("Child nodes: ", mainTitle.childNodes);
+console.log("Child Nodes count: ", mainTitle.childNodes.length);
 
